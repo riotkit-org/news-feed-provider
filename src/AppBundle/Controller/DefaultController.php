842f7fd02c5ad882b55ea869b576d9a5864f5bc7
Damian Kęska <damian.keska@fingo.pl>
2018-07-11 07:23:13 +0200

    Removed not used files and added not found controller for production

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,11 +5,17 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
         return new JsonResponse(['message' => 'Hello'], 200);
+    }
+
+    public function notFoundAction(): void
+    {
+        throw new NotFoundHttpException();
     }
 }
